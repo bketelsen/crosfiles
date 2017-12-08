cf48cf74d1e71e64f92527a9f2056335c094fa8b
Brian Ketelsen <bketelsen@gmail.com>
2017-12-08 19:13:53 +0000

    bootstrap linux

---
+++ bin/1-packages.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+set -e
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get install -y openssh-server \
+      ca-certificates curl unzip tar \
+# Install development packages
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y python3-pip python3-dev python-pip build-essential git-core mercurial bzr python-dev ctags cmake software-properties-common python-software-properties
+
+
---
+++ bin/3-sudo-go.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+set -e
+rm -rf /usr/local/go
+mkdir -p /usr/local/go && curl -Ls https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz | tar xvzf - -C /usr/local/go --strip-components=1
+
+
